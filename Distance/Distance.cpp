1b29a7edcb4c21a26d67e6a220d160cda4db4fef
Alexandr Shapovalov <a.a.shapovalov@bk.ru>
2023-01-18 17:40:58 +0300

    Added a function for calculating the distance between two points on the plane

---
+++ Distance/Distance.cpp
@@ -0,0 +1,67 @@
+﻿// Distance.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "iostream"
+#include <cmath>
+
+
+class Point 
+{
+
+private:
+
+    double x;
+    double y;
+
+public:
+
+    double GetX()
+    {
+        return x;
+    }
+
+    void SetX(double valueX)
+    {
+
+        x = valueX;
+    }
+
+    double GetY()
+    {
+        return y;
+    }
+
+    void SetY(double valueY)
+    {
+
+        y = valueY;
+    }
+
+    double Distance(Point A, Point B)
+    {
+        return sqrt(pow((A.x - B.x), 2) + (pow((A.y - B.y), 2)));
+    }
+
+};
+
+int main()
+{
+  
+    Point A;                    
+
+    A.SetX(2);
+    A.SetY(4);
+
+    Point B;
+    B.SetX(0);
+    B.SetY(0);
+
+    std::cout << "Point A: " << A.GetX() << "\t" << A.GetY() <<"\n";
+    std::cout << "Point B: " << B.GetX() << "\t" << B.GetY() << "\n";
+    std::cout << "Distance beetween two points : " << A.Distance(A, B);
+
+
+}
+
+
+
