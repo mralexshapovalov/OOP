60819e20ed6120cb7fec922ccb9b1c5ff3b2c4b9
Alexandr <a.a.shapovov@bk.ru>
2023-01-24 12:31:03 +0300

    Added method

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -1,182 +1,181 @@
-﻿// OOP.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include "iostream"
+﻿
+//IntroductionToOOP
+#include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
-//Êëàññ - ýòî òèï äàííûõ 
-//Ñòðóêòóðà - ýòî òèï äàííûõ
+#define delimiter "\n-----------------------------------------------\n"
 
-#ifdef STRICT_POINT //Working 16.01.2023
-class Point //Ñîçäàâàÿ ñòóðêòóðó èëè êëàññ, ìû ñîçäàåì íîâûé òèï äàííûõ.
-    //Êëàññû è ñòðóêòóðû åùå íàçûâàþò ïîëüçîâàòåëüñêèìè (ñîñòàâíûìè) òèïàìè äàííûõ
+//				КЛАСС - ЭТО ТИП ДАННЫХ!!!
+//				СТРУКТУРА - ЭТО ТИП ДАННЫХ!!!
 
-{
-
+class Point	//Создавая структуру или класс, мы создаем новый тип данных.
+{			//Классы и структуры еще называют пользовательскими (составными) типами данных.
+	double x;
+	double y;
 public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
 
-    double x;
-    double y;
+	//					Constructors:
+	/*Point()
+	{
+		x = y = 0;
+		//RAII - Resource Aqulisation - Is Initialisation
+		//		(Выделение ресурсов - значит инициализация)
+		cout << "DefConstructor:\t" << this << endl;
+	}*/
+	/*Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "1ArgConstructor:" << this << endl;
+	}*/
+	Point(double x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t" << this << endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t" << this << endl;
+	}
 
-};
-#endif // DEBUG
+	//					Operators:
+	Point operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
 
-#ifdef CONSTRUCTOR_CHEK
-class Point
-{
-private:
-    double x;
-    double y;
-
-public:
-
-    //Point()
-    //{
-    //    x = y = 0;
-    //    //RAII -Resource Aqulisation(âûäåëåíèíå ðåñóðñîâ -çíà÷èò èíöèëèçàöèÿ)
-    //    cout << "Constructro\t" << this<<endl;
-    //}
-    //Point(double x) 
-    //{
-
-    //    this->x = x;
-    //    this->y = 0;
-    //    cout << "!ArgConstructor" << this << endl;
-
-    //}
-
-    Point(const Point& other)
-    {
-        this->x = other.x;
-        this->y = other.y;
-
-        cout << "Конструктор копирования" << this << endl;
-
-    }
-
-    Point(double x = 0, double y = 0)
-    {
-
-        this->x = x;
-        this->y = y;
-        cout << "Constructror\t" << this << endl;
-    }
-
-    ~Point()
-    {
-        cout << "Deconstructro\t" << this << endl;
-
-    }
-    void Print()const
-    {
-        cout << "X=" << x << "\tY=" << y << endl;
-    }
-
-};
-
-#endif // DEBUG
-
-class Point
-{
-private:
-    double x;
-    double y;
-
-public:
-
-    //Point()
-    //{
-    //    x = y = 0;
-    //    //RAII -Resource Aqulisation(âûäåëåíèíå ðåñóðñîâ -çíà÷èò èíöèëèçàöèÿ)
-    //    cout << "Constructro\t" << this<<endl;
-    //}
-    //Point(double x) 
-    //{
-
-    //    this->x = x;
-    //    this->y = 0;
-    //    cout << "!ArgConstructor" << this << endl;
-
-    //}
-
-    Point(const Point& other) 
-    { 
-      this->x = other.x; 
-      this->y = other.y; 
-
-    cout << "Конструктор копирования" << this << endl; 
-    
-    }
-
-    Point(double x = 0, double y = 0)
-    {
-
-        this->x = x;
-        this->y = y;
-        cout << "Constructror\t" << this << endl;
-    }
-
-    ~Point()
-    {
-        cout << "Deconstructro\t" << this << endl;
-
-    }
-    // Operators
-    void operator=(const Point& other) {
-
-
-        this->x = other.x;
-        this->y = other.y;
-
-        cout << "CopyAssingment:\t" << this << endl;
-    }
-
-
-    void Print()const
-    {
-        cout << "X=" << x << "\tY=" << y << endl;
-    }
-
+	//					Methods:
+	double distance(const Point& other)const
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
+	void print()const
+	{
+		cout << "X = " << x << "\tY = " << y << endl;
+	}
 };
 
-
-int main()
+double distance(const Point& A, const Point& B)
 {
-    setlocale(LC_ALL, "RUS");
-
-
-//#ifdef STRUCT_POINT//Working 16.01.2023
-//#endif // DEBUG
-
-//#ifdef CONSTRUCTOR_CHEK
-//Point A;
-//A.Print();
-//
-//Point B(2, 4);
-//B.Print();
-//
-//Point C = 4;
-//C.Print();
-//
-//Point D = C;
-//D.Print();
-
-  /*  Point A(2, 3);
-    A.Print();
-    Point B = A;
-    B.Print();
-    Point C;
-    C = B;
-    C.Print();*/
-
-    Point A, B, C;
-    A = B = C = Point(2, 2);
-
-
-//#endif // CONSTRUCTOR_CHEK{}
-
-
-
-
-
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	return distance;
 }
+
+//#define STRUCT_POINT
+//#define DISTANCE_CHECK
+//#define CONSTRUCTORS_CHECK
+//#define ASSIGNMENT_CHECK_1
+#define ASSIGNMENT_CHECK_2
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	//cout << "Hello OOP" << endl;
+
+#ifdef STRUCT_POINT
+	int a;		//Объявление переменной 'a' типа 'int'
+	Point A;	//Объявление переменной 'A' типа 'Point'
+	//Объявление объекта 'A' структуры 'Point'
+	//Создание экземпляра 'A' структуры 'Point'
+//Объекты часто называют экземплярами класса или структуры.
+//A.x = 2;
+//A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+	//p  - Pointer
+	//pA - Pointer to 'A'
+	//https://ru.wikipedia.org/wiki/%D0%92%D0%B5%D0%BD%D0%B3%D0%B5%D1%80%D1%81%D0%BA%D0%B0%D1%8F_%D0%BD%D0%BE%D1%82%D0%B0%D1%86%D0%B8%D1%8F  
+#endif // STRUCT_POINT
+
+#ifdef DISTANCE_CHECK
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние от точки A до точки B: " << A.distance(B) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние от точки B до точки A: " << B.distance(A) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние между точками A и B:   " << distance(A, B) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние между точками B и A:   " << distance(B, A) << endl;
+	cout << delimiter << endl;
+#endif // DISTANCE_CHECK
+
+#ifdef CONSTRUCTORS_CHECK
+	Point A;	//Default constructor
+	//cout << A.get_x() << "\t" << A.get_y() << endl;
+	A.print();
+
+	Point B(2, 3);
+	B.print();
+
+	Point C = 4;//Single-Argument constructor
+	C.print();
+
+	Point D = C;//Copy constructor
+	//Deep copy - побитовое копирование, побайтовое копирование, поэлементное копирование.
+	//Shallow copy - поверхностное копирование, т.е., копирование адреса памяти, вместо содержимого памяти.
+	D.print();
+#endif // CONSTRUCTORS_CHECK
+
+#ifdef ASSIGNMENT_CHECK_1
+	//CopyAssignment
+	Point A(2, 3);
+	A.print();
+	Point B = A;	//CopyConstructor
+	B.print();
+	Point C;
+	C = B;			//Assignment operator
+	C.print();
+#endif // ASSIGNMENT_CHECK_1
+
+	int a, b, c;
+	a = b = c = 0;
+
+	Point A, B, C;
+	A = B = C = Point(2, 3);
+}
+
