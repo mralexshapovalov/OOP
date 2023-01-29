401920fdadc1516b248e29047f90cfbd26a7098c
Alexandr Shapovalov <a.a.shapovalov@bk.ru>
2023-01-29 22:31:40 +0300

    Started working with operator overloads

--- Franction/Franction.cpp
+++ Franction/Franction.cpp
@@ -2,13 +2,30 @@
 //
 
 #include <iostream>
+
 using namespace std;
 
-class Franction {
+//В классе Fraction реализовать :
+//1. Конструкторы и вывод на экран;
+//5;		//double a = 5;
+//1 / 2;
+//2(3 / 4);
+//
+//2. Написать методы :
+//Fraction & to_proper();	//целую часть интегрирует в числитель
+//Fraction& to_improper();//выделяет целую часть из числителя
+//3. Перегрузить арифметические операторы : +, -, *, / ;
+//4. Перегрузить операторы++ / --;
+//5. Перегрузить составные присваивания : +=, -=, *=, /=;
+//6. Перегрузить операторы сравнения : == , != , > , < , >= , <= ;
+//7. Перегрузить оператор ввода с клавиатуры;
+
+
+class Fraction {
 
 
     int integer;        //целая часть
-    int numerator;     //знаменатель
+    int numerator;      //Числитель
     int denominator;   //знаменатель
 
 
@@ -46,13 +63,98 @@ public :
         this->denominator = denominator;
 
     }
+
+    Fraction(int integer) 
+    {
+        this->integer = integer;
+        this->numerator = 0;
+        this->denominator = 0;
+
+        cout << integer << endl;
+
+    }
+
+    Fraction(int integer,int numerator, int denominator)
+    {
+        this->integer = integer;
+        this->numerator = numerator;
+        this->denominator = denominator;
+
+        cout << integer << numerator<< denominator << endl;
+
+    }
+    Fraction(int numerator, int denominator)
+    {
+        this->integer = integer;
+        this->numerator = numerator;
+        this->denominator = denominator;
+
+        cout << numerator << denominator << endl;
+
+    }
+    ~Fraction()
+    {
+        cout << integer<< numerator << denominator << endl;
+    }
+
+    Fraction& to_proper() 
+    {
+
+
+
+    }
+    Fraction& to_improper()
+    {
+
+
+
+    }
+
+
 };
+
+Fraction operator+(const Fraction& left, const Fraction& right)
+{
+
+    Fraction res;
+
+
+    return res;
+}
+Fraction operator-(const Fraction& left, const Fraction& right)
+{
+
+    Fraction res;
+
+
+    return res;
+}
+Fraction operator*(const Fraction& left, const Fraction& right)
+{
+
+    Fraction res;
+
+
+    return res;
+}
+Fraction operator/(const Fraction& left, const Fraction& right)
+{
+
+    Fraction res;
+
+
+    return res;
+}
+
 
 
 
 int main()
 {
-    
+    Fraction A = 5;
+    Fraction B(2, 3);
+    Fraction C(1,2, 3);
+ 
 }
 
 
