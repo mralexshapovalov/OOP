b0027279eada953f7dccf323958a5a009cd9a66a
Alexandr Shapovalov <a.a.shapovalov@bk.ru>
2023-02-27 21:28:38 +0300

    Geometry project

---
+++ Geometry/Geometry.cpp
@@ -0,0 +1,41 @@
+﻿// Geometry.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+enum Color
+{
+
+    ConsoleBlue = 0x99,
+    ConsoleGreen = 0xAA,
+    ConsoleRED=0xCC,
+    ConsoleDefault=0x07
+
+};
+
+class Shape 
+{
+    Color color;
+public:
+    Shape(Color color) :color(color) {};
+
+    virtual ~Shape(){}
+
+    virtual double ger_area()const = 0;
+    virtual double get_perimetr()const = 0;
+    virtual void draw()const = 0;
+
+
+
+};
+
+
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+
+
+}
+
